fcdc3e0cd08ef6e274516d4919a306a9d84a9ba2
NickMozav <nick.mr.volk@gmail.com>
2024-07-23 13:00:38 +0300

    prod fix mails module path

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 import { TariffsModule } from './tariffs/tariffs.module'
 import { LessonsModule } from './lessons/lessons.module'
-import { MailsModule } from './mails/Mails.module'
+import { MailsModule } from './mails/mails.module'
 
 @Module({
     imports: [ConfigModule.forRoot(), AuthModule, UsersModule, TariffsModule, LessonsModule, MailsModule],
